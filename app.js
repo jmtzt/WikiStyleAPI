9b4cf2f79b677346001c9255a826a402574401e1
jmtzt <jmttozato@gmail.com>
2020-04-21 17:07:42 -0300

    implemented get/post/delete for all articles

--- app.js
+++ app.js
@@ -23,6 +23,46 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.route("/articles").get(function (req, res) {
+
+    Article.find(function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        }
+        else {
+            res.send(err);
+        }
+    });
+}).post(function (req, res) {
+
+    const article = new Article({ title: req.body.title, content: req.body.content });
+
+    article.save(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send("Article added sucessfully!");
+        }
+    });
+
+
+}).delete(function (req, res) {
+    Article.deleteMany(function (err) {
+        if (!err) {
+            res.send("Deleted all articles sucessfully!");
+        }
+        else {
+            res.send(err);
+        }
+    })
+})
+
+
+
+
+
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
